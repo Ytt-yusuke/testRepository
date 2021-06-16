9069f43cc1407343347715134da5a3ea10374826
Yttlium <yusuke19991011@gmail.com>
2021-06-17 08:36:27 +0900

    update

--- paramSetGame/Assets/test/boxMove.cs
+++ paramSetGame/Assets/test/boxMove.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class boxMove : MonoBehaviour
 {
     private Rigidbody2D RB2;
+    private bool isFirst;
+
     // Start is called before the first frame update
     void Start()
     {
         RB2 = GetComponent<Rigidbody2D>();
+        isFirst = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RB2.AddForce(new Vector2(-1, 1));
+        if (isFirst == false)
+        {
+            RB2.AddForce(new Vector2(-5, 1), ForceMode2D.Impulse);
+            isFirst = true;
+        }
 
         if (transform.position.y <= -20)
         {
